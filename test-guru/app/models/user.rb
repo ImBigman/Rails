da985ad60e282ad483d8d9e85c42e69119b5975d
ImBigman <b1gmanever@gmail.com>
2019-11-28 21:12:03 +0300

    3rdLesson

--- test-guru/app/models/user.rb
+++ test-guru/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def tests_history(level)
+    history = Test.where(level: level)
+  end
 end
--- test-guru/db/seeds.rb
+++ test-guru/db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([
+                         { first_name: 'Donatello', second_name: 'Splinters', login: 'Donny', email: 'ninja_turtles_1@gmail.com' },
+                         { first_name: 'Raphael', second_name: 'Splinters', login: 'Raf', email: 'ninja_turtles_2@gmail.com' },
+                         { first_name: 'Michelangelo', second_name: 'Splinters', login: 'Mike', email: 'ninja_turtles_3@gmail.com' },
+                         { first_name: 'Leonardo', second_name: 'Splinters', login: 'Leo', email: 'ninja_turtles_4@gmail.com' }
+                    ])
+categories = Category.create([
+                                  { title: 'Backend' },
+                                  { title: 'Frontend' },
+                                  { title: 'Full stack' }
+                             ])
+tests = Test.create([
+                         { title: 'CSS', level: 1, category_id: categories[1].id },
+                         { title: 'HTML', level: 0, category_id: categories[1].id },
+                         { title: 'Basics of JS', level: 1, category_id: categories[1].id },
+                         { title: 'Ruby on Rails', level: 3, category_id: categories[0].id },
+                         { title: 'Package managers', level: 2, category_id: categories[2].id }
+                    ])
+questions = Question.create([
+                                { body: 'Basic SEO', test_id: tests[1].id },
+                                { body: 'Box model', test_id: tests[0].id },
+                                { body: 'DOM manipulation', test_id: tests[2].id },
+                                { body: 'MVC on Rails', test_id: tests[3].id },
+                                { body: 'npm', test_id: tests[4].id }
+                            ])
+answers = Answer.create([
+                            { body: 'Meta description tag', correct: true, question_id: questions[0].id },
+                            { body: 'Properties controlling the flow of content in a box', correct: true, question_id: questions[1].id },
+                            { body: 'querySelector(), addEventListener()', correct: true, question_id: questions[2].id },
+                            { body: 'Each component of the model-view-controller architecture has its place within the app subdirectory', correct: true, question_id: questions[3].id },
+                            { body: 'Check for packages not declared in package.json', correct: true, question_id: questions[4].id }
+                        ])
