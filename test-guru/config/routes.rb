00d7a9dc5c3e200075e573f47d0b60d6e55c0551
ImBigman <b1gmanever@gmail.com>
2019-12-06 14:07:32 +0300

    add controllers fot tests and questions

--- test-guru/config/routes.rb
+++ test-guru/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'tests#index'
 
-  # get 'about/author', to: 'author#index'
-  # get 'about', to: 'about#index'
-  #
-  # root 'about#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tests do
+    resources :questions, shallow: true
+  end
 end
