5cf0e01b20be97c67692f601959f1c6385099410
ImBigman <b1gmanever@gmail.com>
2019-12-12 11:42:44 +0300

    add before_update method and relocate success? method

--- test-guru/app/models/test_passage.rb
+++ test-guru/app/models/test_passage.rb
@@ -3,18 +3,25 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_update :next_question
   before_validation :before_validation_set_first_question, on: :create
+
+  BOUNDARY_OF_SUCCESS = 85.freeze
+
+  before_update do
+    self.current_question = next_question
+  end
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-
-    self.current_question = next_question
     save!
   end
 
   def completed?
     current_question.nil?
+  end
+
+  def success?
+    percentage_result < BOUNDARY_OF_SUCCESS
   end
 
   def percentage_result
