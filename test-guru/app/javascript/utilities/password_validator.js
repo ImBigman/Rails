e287973820bc258f034d284b9cace00beb8fe3ac
ImBigman <b1gmanever@gmail.com>
2019-12-24 22:55:50 +0300

    cosmetic in js file

--- test-guru/app/javascript/utilities/password_validator.js
+++ test-guru/app/javascript/utilities/password_validator.js
@@ -6,11 +6,12 @@ document.addEventListener('turbolinks:load', () => {
 function validatePassword() {
     let enter_password = document.querySelector('.password'),
         confirm_password = document.querySelector('.confirm-password');
-            if (enter_password.value !== confirm_password.value) {
-            document.querySelector('.text-danger').classList.remove('hide')
-        } else {
-            document.querySelector('.text-danger').classList.add('hide');
-                document.querySelector('.check').classList.remove('hide')
-        }
+    if (enter_password.value !== confirm_password.value) {
+
+        document.querySelector('.text-danger').classList.remove('hide')
+    } else {
+        document.querySelector('.text-danger').classList.add('hide');
+        document.querySelector('.check').classList.remove('hide')
+    }
 }
 
